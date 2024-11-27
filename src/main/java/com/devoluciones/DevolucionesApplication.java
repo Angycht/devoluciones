7e17a30fb8dcdf3bf695df46dec8c16167a7d2b9
Ángela Charte Torres <158575413+Angycht@users.noreply.github.com>
2024-11-27 13:15:37 +0100

    d

---
+++ src/main/java/com/devoluciones/DevolucionesApplication.java
@@ -0,0 +1,13 @@
+package com.devoluciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevolucionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevolucionesApplication.class, args);
+	}
+
+}
